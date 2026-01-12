9bbc87464d19e496c0b0ba604dc43c3b49282f05
Jhnrfael <john.mendozatovera@elev.ga.lbs.se>
2026-01-12 10:52:01 +0100

    Complete Puzzle 33 script

--- Grupp 2.14/Assets/Scenes/Puzzle 3/Scripts/Drag Control.cs
+++ Grupp 2.14/Assets/Scenes/Puzzle 3/Scripts/Drag Control.cs
@@ -1,28 +1,62 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class DragControl : MonoBehaviour
 {
 
-    [SerializeField] Rigidbody rb;
+    [SerializeField] Rigidbody2D rb;
     [SerializeField] GameObject wordBox;
+    [SerializeField] GameObject correctBlankSpace;
 
     private Vector3 ogWorldBoxPos;
+    private bool isPlaced = false;
+    private bool isCorrectlyPlaced = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
         ogWorldBoxPos = wordBox.transform.position;
     }
 
     void OnMouseDrag()
     {
+        if (isPlaced) return;
+
         rb.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == correctBlankSpace)
+        {
+            isCorrectlyPlaced = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject != correctBlankSpace)
+        {
+            isCorrectlyPlaced = false;
+        }
     }
 
     void OnMouseUp()
     {
-        rb.position = ogWorldBoxPos;
+        if (isPlaced) return;
+
+        if (isCorrectlyPlaced)
+        {
+            rb.position = correctBlankSpace.transform.position;
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            rb.isKinematic = true;
+            isPlaced = true;
+        }
+        else
+        {
+            rb.position = ogWorldBoxPos;
+        }
     }
 }
